9abda751268998717a50d93648fadbda9b461f82
simon <simonbw@uw.edu>
2016-06-19 11:52:31 -0700

    Color Util Functions

--- src/util/Util.js
+++ src/util/Util.js
@@ -23,3 +23,46 @@ export function angleDelta(a, b) {
 export function length([x, y]) {
   return Math.sqrt(x * x + y * y);
 }
+
+export function rgbToHex(red, green, blue) {
+  return clamp(blue, 0, 255) + (clamp(green, 0, 255) << 8) + (clamp(red, 0, 255) << 16);
+}
+
+export function rgbObjToHex({r, g, b}) {
+  return rgbToHex(r, g, b);
+}
+
+export function hexToRGB(hex) {
+  return {
+    r: (hex >> 16),
+    g: ((hex >> 8) & 0x0000FF),
+    b: (hex & 0x0000FF)
+  }
+}
+
+// given colors "from" and "to", return a hex array [from, x, y, z, to]
+// where there are exactly (steps + 1) elements in the array
+// and each element is a color that fades between the two endpoint colors
+export function colorRange(from, to, steps) {
+  const perStepFade = 1.0 / steps;
+  const out = [];
+  for (let i = 0; i < steps; i++) {
+    out.push(colorFade(from, to, perStepFade * i));
+  }
+  return out;
+}
+
+export function colorFade(from, to, percentFrom) {
+  const rgbFrom = hexToRGB(from);
+  const rgbTo = hexToRGB(to);
+
+  rgbFrom.r = Math.floor(rgbFrom.r * percentFrom);
+  rgbFrom.g = Math.floor(rgbFrom.g * percentFrom);
+  rgbFrom.b = Math.floor(rgbFrom.b * percentFrom);
+
+  rgbTo.r = Math.floor(rgbTo.r * (1 - percentFrom));
+  rgbTo.g = Math.floor(rgbTo.g * (1 - percentFrom));
+  rgbTo.b = Math.floor(rgbTo.b * (1 - percentFrom));
+
+  return rgbObjToHex(rgbFrom) + rgbObjToHex(rgbTo);
+}
